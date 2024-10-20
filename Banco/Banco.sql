f3a9da70bfdb1b42742fe5141768ebe988f53ce4
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-10-20 15:59:00 -0300

    Add files via upload

--- Banco/Banco.sql
+++ Banco/Banco.sql
@@ -2,8 +2,6 @@ create database banco;
 
 use banco;
 
-drop database banco;
-
 create table banco(
 
     codigo int auto_increment primary key,
@@ -12,8 +10,6 @@ create table banco(
 );
 insert into banco (nome) values('Banco do Brasil');
 insert into banco (nome) values('CEF');
-
-drop table agencia;
 
 create table agencia(
  
@@ -26,14 +22,18 @@ create table agencia(
 );
 insert into agencia (numero_agencia , endereco , cod_banco) values(0562 , 'Rua Joaquim Teixeira Alves, 1555' , 1);
 insert into agencia (numero_agencia , endereco , cod_banco) values(3153 , 'Av. Marcelino Pires, 1960' , 2);
-/*    3    update agencia
-              set numero_agencia = '6342'
-              where numero_agencia = '0562';*/
-/*    6    insert into agencia (numero-agencia , endereco , cod_banco) values(1333 , 'Rua João José da Silva, 486' , 1);*/
 
-/*    7    select numero_agencia, endereco
-              from agencia
-              where cod_banco = 1;*/
+/*3*/ ALTER TABLE conta DROP FOREIGN KEY conta_ibfk_1;
+      ALTER TABLE conta ADD CONSTRAINT conta_ibfk_1 FOREIGN KEY (num_agencia) REFERENCES agencia (numero_agencia) ON UPDATE CASCADE;
+      update agencia
+      	set numero_agencia = 6342
+    	where numero_agencia = 0562 and cod_banco = 1;
+
+/*6*/ insert into agencia (numero_agencia , endereco , cod_banco) values(1333 , 'Rua João José da Silva, 486' , 1);
+
+/*7*/ select numero_agencia, endereco
+		from agencia
+		where cod_banco = 1;
     
 create table conta(
 
@@ -46,11 +46,11 @@ create table conta(
 );
 insert into conta (num_conta , saldo , tipo_conta , num_agencia) values('86340-2' , 763.05 , '2' , 3153);
 insert into conta (num_conta , saldo , tipo_conta , num_agencia) values('23584-7' , 3879.12 , '1' , 0562);
-/*     5     update conta
-                set saldo = saldo * 0.10
-                where num_conta = '23584-7';*/
+/*5*/ update conta
+	    set saldo = saldo * 0.10
+	    where num_conta = '23584-7';
 
-/*     9     DELETE FROM `banco`.`conta` WHERE (`num-conta` = '86340-2');*/
+/*9*/   DELETE FROM conta WHERE num_conta = '86340-2';
 
 create table cliente(
 
@@ -60,20 +60,21 @@ create table cliente(
     sexo char(1)
 
 );
-insert into cliente (cpf , nome , endereco , sexo ) values('111.222.333-44' , 'Jennifer B Souza' , 'Rua Cuiabá' , 'F');
+insert into cliente (cpf , nome , endereco , sexo ) values('111.222.333-44' , 'Jennifer B Souza' , 'Rua Cuiabá, 1050' , 'F');
 insert into cliente (cpf , nome , endereco , sexo ) values('666.777.888-99' , 'Caetano K Lima' , 'Rua Ivinhema, 879' , 'M');
-insert into cliente (cpf , nome , endereco , sexo ) values('555.444.333-22' , 'Silvia Macedo' , 'Rua Estados Unidos, 735' , 'F');
-/*    1     alter table cliente add column email varchar(200);*/
-/*    2     select cpf, endereco
-               from cliente
-               where nome = 'c%';*/
-/*    4     update cliente
-               set email = 'caetanolima@gmail.com'
-               where nome = 'Caetano K Lima';*/
+insert into cliente (cpf , nome , endereco , sexo ) values('555.444.777-33' , 'Silvia Macedo' , 'Rua Estados Unidos, 735' , 'F');
+/*1*/ alter table cliente add column email varchar(200);
+/*2*/ select cpf, endereco
+         from cliente
+         where nome like 'C%';
+                 
+/*4*/ update cliente
+      	set email = 'caetanolima@gmail.com'
+    	where cpf = '666.777.888-99';
 
-/*    8     select *
-               from cliente
-               where sexo = 'M';*/
+/*8*/ select *
+		from cliente
+		where sexo = 'M';
 
 DELETE FROM `banco`.`cliente` WHERE (`cpf` = '666.777.888.99');
 
@@ -89,7 +90,14 @@ create table historico(
 );
 insert into historico (cpf , num_conta , data_inicio) values('111.222.333-44' , '23584-7' , '1997-12-17');
 insert into historico (cpf , num_conta , data_inicio) values('666.777.888-99' , '23584-7' , '1997-12-17');
-insert into historico (cpf , num_conta , data_inicio) values('555.444.333-22' , '86340-2' , '2010-11-29');
+insert into historico (cpf , num_conta , data_inicio) values('555.444.777-33' , '86340-2' , '2010-11-29');
+
+
+/*9*/ ALTER TABLE historico DROP FOREIGN KEY historico_ibfk_2;
+      ALTER TABLE historico
+	     ADD CONSTRAINT historico_ibfk_2 FOREIGN KEY (num_conta) 
+         REFERENCES conta(num_conta) ON DELETE CASCADE;
+
 
 create table telefone_cliente(
     
@@ -101,5 +109,4 @@ create table telefone_cliente(
 );
 insert into telefone_cliente (cpf_cli , telefone) values('111.222.333-44' , '(67)3422-7788');
 insert into telefone_cliente (cpf_cli , telefone) values('666.777.888-99' , '(67)3423-9900');
-insert into telefone_cliente (cpf_cli , telefone) values('555.444.333-22' , '(67)8121-8833');
-
+insert into telefone_cliente (cpf_cli , telefone) values('666.777.888-99' , '(67)8121-8833');
