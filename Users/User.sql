e902f70cf2c2b86580b4555fdf5e6d9fcb08d2d1
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-12-16 21:57:54 -0300

    Update User.sql

--- Users/User.sql
+++ Users/User.sql
@@ -1,25 +1,25 @@
 -- 1
-Create USER 'my_user1' identified by 'user123';
+Create USER 'my_user1'@'localhost' identified by 'user123';
 
 -- 2
-Create USER 'my_admin' identified by 'admin123';
+Create USER 'my_admin'@'localhost' identified by 'admin123';
 
 -- 3
 use bd_empresa;
 use auladml;
 
 -- 4
-GRANT ALL ON bd_empresa.* TO 'my_admin';
+GRANT ALL ON bd_empresa.* TO my_admin;
 
 -- 5
-GRANT select, insert, delete, update ON bd_empresa.*TO 'my_user1';
+GRANT select, insert, delete, update ON bd_empresa.*TO my_user1;
 
 -- 6
-SHOW GRANTS FOR 'my_user1';
-SHOW GRANTS FOR 'my_admin';
+SHOW GRANTS FOR my_user1;
+SHOW GRANTS FOR my_admin;
 
 -- 7
-REVOKE ALL ON *.* from 'my_user1';
+REVOKE ALL ON bd_empresa.* from 'my_user1';
 
 -- 8
 -- Esse códgio foi feito no usuário my_user1 após eu conceder permissão novamente ao bd_empresa
@@ -37,8 +37,8 @@ CREATE ROLE papelAdmin;
 CREATE ROLE papelDev;
 
 -- 11
-GRANT ALL ON *.* TO papelAdmin;
-GRANT select, insert, update, delete ON *.* TO papelDev;
+GRANT ALL ON universidade.* TO papelAdmin;
+GRANT select, insert, update, delete ON universidade.* TO papelDev;
 
 -- 12
 CREATE USER 'adm' identified by 'adm123';
@@ -47,7 +47,8 @@ CREATE USER 'dev' identified by 'dev123';
 GRANT papelAdmin TO 'adm';
 GRANT papelDev TO 'dev';
 
+set default role papelAdmin to adm;
+set default role papelDev to dev;
+
 show grants for papelAdmin;
 show grants for papelDev;
-
-
